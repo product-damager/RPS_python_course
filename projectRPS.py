e05ed6f001b59683b681ab1731395c874f528bbd
Vireo <154900180+product-damager@users.noreply.github.com>
2023-12-26 17:28:53 +0100

    Add file with program

---
+++ projectRPS.py
@@ -0,0 +1,31 @@
+import random
+def get_choices():
+    player_choice = input("Enter a choice: ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices_dict = {"player": player_choice, "computer": computer_choice}
+    return choices_dict
+
+def check_win(player, computer):
+    print(f"You chose {player}. Computer chose {computer}.")
+    if player == computer:
+        return "Tie." 
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock beats scissors. You win!"
+        elif computer == "paper":
+            return "Paper beats rock. You lose."
+    elif player == "paper":
+        if computer == "scissors":
+            return "Scissors beats paper. You lose."
+        elif computer == "rock":
+            return "Paper beats rock. You win!"
+    elif player == "scissors":
+        if computer == "rock":
+            return "Rock beats scissors. You lose."
+        elif computer == "paper":
+            return "Scissors beats paper. You win!"
+
+choices = get_choices()
+result = check_win(choices["player"],choices["computer"])
+print(result)
